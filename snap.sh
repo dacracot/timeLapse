7e8075c0107a6a33ab5fa23930f64faf702c102c
David Craig Cothren <dacracot@yahoo.com>
2023-12-14 13:56:12 -0800

    more... orient the execution directory

--- snap.sh
+++ snap.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+# orient the execution directory
+if [ -z "$WHEREAMI" ]; then
+    pushd .. > /dev/null
+    export WHEREAMI=$PWD
+    popd > /dev/null
+fi
 # get the new number from the file storage
-COUNTER=$[$(cat /home/dacracot/spy/sequence) + 1]
+COUNTER=$[$(cat $WHEREAMI/sequence) + 1]
 # get an eight digit number
 SEQ=$(printf "%08d" $COUNTER)
 # push the counter back to the file storage
-echo $COUNTER > /home/dacracot/spy/sequence
+echo $COUNTER > $WHEREAMI/sequence
 # set the filename
-FILENAME=/home/dacracot/spy/shots/shot-$SEQ.jpg
+FILENAME=$WHEREAMI/shots/shot-$SEQ.jpg
 # take the picture with a timestamp
 raspistill -awb auto --exposure auto --ev 0 --rotation 270 --output $FILENAME --encoding jpg --quality 80 --nopreview --timeout 1000 --annotate 8 --annotate "%Y-%m-%d %H:%M"
 mogrify -format png -thumbnail 320x320 $FILENAME
