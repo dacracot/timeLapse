4259f3d52a4b78b94b5da79a44da7615750a1aeb
David Craig Cothren <dacracot@yahoo.com>
2023-12-17 10:22:49 -0800

    add video creation after shot completion

--- stop.sh
+++ stop.sh
@@ -7,3 +7,9 @@ kill -15 `cat monitor.pid`
 sleep 4
 # check if it is still running
 ps -elf | grep monitor
+echo "---------------------"
+echo "-- creating video"
+WHEN=$(date '+%Y%m%d%H%M')
+ffmpeg -framerate 60 -i shots/shot-%08d.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p timeLapse-$WHEN.mp4
+echo "---------------------"
+ls -ltr timeLapse-*.mp4
