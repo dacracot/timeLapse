4ba77ba46489f74d863f6911e2c6f0d357f4bb8d
David Craig Cothren <dacracot@yahoo.com>
2023-12-18 14:41:31 -0800

    guide for different frame rates

---
+++ frameRate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "---------------------"
+# set delay between pictures
+echo "What is your primary subject?"
+echo
+select rate in people scene plant
+do
+	case $rate in
+		people)
+			echo "10" > DELAY
+			break
+			;;
+		scene)   
+			echo "60" > DELAY
+			break
+			;;
+        plant)   
+			echo "120" > DELAY
+			break
+			;;
+        *)
+			echo "Not a valid value." 
+			;;
+		esac
+done
