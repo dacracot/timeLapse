32c3f0a3c49ad21ce9b6dbde296b65247c19d8d4
David Craig Cothren <dacracot@yahoo.com>
2025-10-30 15:41:37 -0700

    issue-7, check disk usage

--- lapse.sh
+++ lapse.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 # ---------------------------------------------------
-{
+PERCENT_STORAGE_ALLOWED=90
+# ---------------------------------------------------
 # orient the execution directory
 if [ -z "$WHEREAMI" ]; then
     export WHEREAMI=$PWD
 fi
+# ---------------------------------------------------
+function calculatePercentageUsed {
+	ROOTDIR=`echo ${WHEREAMI} | cut -d '/' -f 2`
+	if mountpoint -q /${ROOTDIR}; then
+		echo `df -h | grep -oP "\d{1,2}% \/${ROOTDIR}$" | grep -oP "\d{1,2}"`
+	else
+		echo "$(df -h | grep -oP '\d{1,2}% \/$' | grep -oP '\d{1,2}')"
+	fi
+	}
+# ---------------------------------------------------
+{
 # save the PID
 echo $$ > $WHEREAMI/lapse.pid
 # delay in seconds before next loop
@@ -13,8 +25,23 @@ WAIT=$(cat $WHEREAMI/DELAY)
 trap "exit" INT
 # loop every few seconds
 while sleep $WAIT; do
-  {
-  $WHEREAMI/snap.sh 
-  }
+	{
+	# record the image
+	$WHEREAMI/snap.sh 
+	# check how full storage is getting at /
+	PERCENT_STORAGE_USED=$(calculatePercentageUsed)
+	if [ ${PERCENT_STORAGE_USED} -ge ${PERCENT_STORAGE_ALLOWED} ]; then
+		echo "Reached ${PERCENT_STORAGE_ALLOWED}% of storage used.  Terminating recordings."
+		exit
+	fi
+	}
 done
 } >> ${WHEREAMI}/lapse.out 2>> ${WHEREAMI}/lapse.err
+
+
+
+
+
+
+
+
