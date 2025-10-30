4d2e4e6a038c000bae89d667b36e6c548f3cbecc
David Craig Cothren <dacracot@yahoo.com>
2025-10-30 07:40:08 -0700

    png scaled 25% to gif

--- snap.sh
+++ snap.sh
@@ -2,7 +2,7 @@
 # take the picture
 rpicam-still --verbose=0 --immediate --nopreview --encoding png --output /tmp/frame.png
 # convert PNG to GIF
-convert /tmp/frame.png /tmp/frame.gif
+convert -scale 25% /tmp/frame.png /tmp/frame.gif
 # add to animated gif
 gifsicle --colors 256 --batch /var/www/html/timeLapse.gif --append /tmp/frame.gif
 # debug
