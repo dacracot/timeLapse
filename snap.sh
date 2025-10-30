177c248644a8b0db60d5d4f4a38c79cc6b0faa15
David Craig Cothren <dacracot@yahoo.com>
2025-10-30 09:16:42 -0700

    png to jpg, +hdr and scale at capture

--- snap.sh
+++ snap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # take the picture
-rpicam-still --verbose=0 --immediate --nopreview --encoding png --output /tmp/frame.png
+rpicam-still --verbose=0 --immediate --nopreview --width 820 --height 616 --hdr --encoding jpg --output /tmp/frame.jpg
 # convert PNG to GIF
-convert -scale 25% /tmp/frame.png /tmp/frame.gif
+convert /tmp/frame.jpg /tmp/frame.gif
 # add to animated gif
 gifsicle --colors 256 --batch /var/www/html/timeLapse.gif --append /tmp/frame.gif
 # debug
